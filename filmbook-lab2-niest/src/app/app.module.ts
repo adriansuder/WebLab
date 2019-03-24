d8d88b0a72d06ff7c303122f2f777f2a86bcac97
Ryszard Brzegowy <rbrzegowy@wsei.edu.pl>
2019-03-24 19:28:16 +0100

    lab2 eod

---
+++ filmbook-lab2-niest/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NewFilmComponent } from './new-film/new-film.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatInputModule } from '@angular/material';
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewFilmComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ filmbook-lab2-niest/src/app/new-film/new-film.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-film',
+  templateUrl: './new-film.component.html',
+  styleUrls: ['./new-film.component.scss']
+})
+export class NewFilmComponent implements OnInit {
+
+  // FormControl, FormArray, FormGroup
+  newFilm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.newFilm = this.fb.group({
+      title: ['', Validators.required],
+      description: [''],
+      rating: ['', Validators.required],
+      poster: [''],
+    });
+  }
+  onDodajFilmClick() {
+    // console.log(this.newFilm.controls.title.value);
+    console.log(this.newFilm);
+  }
+
+}
